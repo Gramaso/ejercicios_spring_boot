9976a3803cbe16b6230dadba1c95be879869bd69
Gramaso <gutierrez.caceres.antonio@iescamas.es>
2024-11-22 09:49:48 +0100

    segundo ejercicio

--- estructuras/src/main/java/com/ejercicio/estructuras/Controlador.java
+++ estructuras/src/main/java/com/ejercicio/estructuras/Controlador.java
@@ -3,18 +3,29 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
+
 @RestController
 public class Controlador {
 @PostMapping("/parImpar")
 public String ejercicio1(@RequestParam int numero) {
     String ret = " ";
-    if(numero%2 == 0){
-        ret = "par";
-    } else {
-        ret = "impar";
-    }
+    ret = numero % 2 == 0?"par":"impar";
     return ret;
 }
+@PostMapping("/tablaMultiplicar")
+public String postMethodName(@RequestParam int numero) {
+    int resultado = 0;
+    String devolver = " ";
+    for (int i = 1 ; i <= 10 ; i++ ){
+        resultado = numero * i;
+        devolver += numero + "x" + i + "=" + resultado + "<br>";
+    }
+        return devolver;
+    
+    
+}
+
 
 
 }
